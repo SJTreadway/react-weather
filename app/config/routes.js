a8aed7ec97c0235db4517c5aee2a59df157da9ba
SJTreadway <treadway08@gmail.com>
2016-04-13 15:18:19 -0500

    adds boilerplate

---
+++ app/config/routes.js
@@ -0,0 +1,17 @@
+var React = require('react');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var Main = require('../containers/Main');
+var HomeContainer = require('../containers/HomeContainer');
+
+var routes = (
+    <Router>
+        <Route path='/' component={Main}>
+            <IndexRoute component={HomeContainer} />
+        </Route>
+    </Router>
+);
+
+module.exports = routes;
